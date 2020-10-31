0e3bf7e9af177969a3285234e42e64b7a2f924d9
Xavier Ripoll <xaviripo97@gmail.com>
2020-10-31 13:08:59 +0100

    Reverse Blog post order

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,7 +1,7 @@
 <script context="module">
 	export function preload({ params, query }) {
 		return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
-			return { posts };
+			return { posts: posts.reverse() };
 		});
 	}
 </script>
