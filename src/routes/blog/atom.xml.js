dc31893335e01b03f36b4d2ede516909b268d8cb
Xavier Ripoll <xaviripo97@gmail.com>
2021-05-22 10:09:10 +0200

    Remove Atom feed comments from output

--- src/routes/blog/atom.xml.js
+++ src/routes/blog/atom.xml.js
@@ -10,37 +10,28 @@ function makeFeed(entries) {
 	// See specification: https://validator.w3.org/feed/docs/atom.html
 	return `<?xml version="1.0" encoding="utf-8"?>
 <feed xmlns="http://www.w3.org/2005/Atom">
-
-	<!-- Mandatory fields -->
-	<id>${domain}blog</id>
+	<id>${domain}blog</id>${ "" /* Mandatory fields */ }
 	<title>${fullName} — Blog</title>
 	<updated>${ entries.sort((a, b) => b - a).pop().date }</updated>
-
-	<!-- Recommended fields -->
+	${ "" /* Recommended fields */ }
 	${author}
 	<link rel="self" href="${domain}blog/atom.xml"/>
-
-	<!-- Optional fields -->
+	${ "" /* Optional fields */ }
 	<icon>/favicon.png</icon>
 	<rights>© ${new Date().getFullYear()} ${fullName} </rights>
 
-	<!-- Entries -->
+${ "" /* Entries */ }
 ${entries
 		.reverse()
 		.map(({ title, slug, date, tags, summary }) => `	<entry>
-		<!-- Mandatory fields -->
-		<id>${domain}blog/${slug}</id>
+		<id>${domain}blog/${slug}</id>${ "" /* Mandatory fields */ }
 		<title>${title}</title>
 		<updated>${date}</updated>
-
-		<!-- Recommended fields -->
-		${author}
+		${author}${ "" /* Recommended fields */ }
 		<content type="text/html" src="${domain}blog/${slug}"/>
 		<link href="${domain}blog/${slug}"/>
 		<summary>${summary}</summary>
-
-		<!-- Optional fields -->
-		${(tags || []).map(tag => `<category term="${tag}"/>`).join('\n')}
+		${(tags || []).map(tag => `<category term="${tag}"/>`).join('\n')}${ "" /* Optional fields */ }
 		<published>${date}</published>
 		<rights>© ${ date.substring(0, 4) } ${fullName}</rights>
 	</entry>`)
